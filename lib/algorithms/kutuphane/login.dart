897d8ab75977ae677949c6530454ad4f0f36a351
natanz <natazdev@gmail.com>
2025-08-23 16:42:10 +0300

    uygulama için gerekli alt yapı oluşturuldu

---
+++ lib/algorithms/kutuphane/login.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_1/algorithms/kutuphane/user.dart';
+
+class LoginPage extends User {
+  LoginPage(super.name, super.username, super.password);
+}
---
+++ lib/algorithms/kutuphane/user.dart
@@ -0,0 +1,7 @@
+class User {
+  final String username;
+  final String password;
+  final String name;
+
+  User(this.name, this.username, this.password);
+}
